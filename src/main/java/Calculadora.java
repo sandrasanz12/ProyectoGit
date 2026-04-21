e5522e4a20843317dd59ba19ee763a86cac6c6c8
sandrasanz12 <sanz37414@gmail.com>
2026-04-21 16:39:46 +0200

    Inicio del proyecto: Calculadoa y estructura básica

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora { 
+    public int sumar(int a, int b) { 
+        return a + b; 
+        } 
+        public int restar(int a, int b) { 
+            return a - b; 
+            } 
+}
