fe843d6edc948c2fe3da267884ced84dd19c91d5
sandrasanz12 <sanz37414@gmail.com>
2026-04-21 16:42:06 +0200

    Calculadora añade el metodo multiplicar

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,8 +1,15 @@
 public class Calculadora { 
+
     public int sumar(int a, int b) { 
         return a + b; 
-        } 
-        public int restar(int a, int b) { 
-            return a - b; 
-            } 
+    } 
+
+    public int restar(int a, int b) { 
+        return a - b; 
+    } 
+
+    // Metodo nuevo que añadimos en el segundo commit 
+    public int multiplicar(int a, int b) { 
+        return a * b; 
+    }
 }
