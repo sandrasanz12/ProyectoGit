9d94ed018160708b17ce3a9331cfea819ad91575
sandrasanz12 <sanz37414@gmail.com>
2026-04-21 16:43:45 +0200

    añade clase Main con demostracion de calculadora

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main { 
+    public static void main(String[] args) { 
+        Calculadora calc = new Calculadora(); 
+        System.out.println("5 + 3 = " + calc.sumar(5, 3)); 
+        System.out.println("5 - 3 = " + calc.restar(5, 3)); 
+        System.out.println("5 * 3 = " + calc.multiplicar(5, 3)); 
+        } 
+}
